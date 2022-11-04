dcf7a16ba79b6d08ec31e1358ff2473dbd75d81c
Tom Welsh <salthook@gmail.com>
2022-11-04 16:58:09 +0000

    added 3col-footer skeleton

---
+++ _INC/FOOTERS/3col-footer/3col-footer.php
@@ -0,0 +1,15 @@
+<footer class='tc-3col-footer'>
+
+    <div class='tc-3col-footer-content-container'>
+        <div class='tc-3col-footer-content-card'>
+
+        </div>
+        <div class='tc-3col-footer-content-card'>
+
+        </div>
+        <div class='tc-3col-footer-content-card'>
+
+        </div>
+    </div>
+    <div class='tc-3col-footer-copyright-bar'></div>
+</footer>
